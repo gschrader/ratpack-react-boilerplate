5c9e548b6d64345ac5e8790d8450ffea9bb9810d
Glen Schrader <glen.schrader@entero.com>
2015-06-22 12:56:59 -0600

    move webpack output to resource directory

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     './src/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'build/resources/main'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
