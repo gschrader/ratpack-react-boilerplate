1b2fa2c4bb7bfae167773cf8e599287d5bda6648
Glen Schrader <glen.schrader@entero.com>
2015-10-22 13:16:42 -0600

    update to use webpack-hot-middleware
    this was the basis
    https://github.com/gaearon/react-transform-boilerplate

---
+++ webpack.config.dev.js
@@ -0,0 +1,48 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr&timeout=20000',
+    './src/main/resources'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new ExtractTextPlugin("bundle.css", {
+      allChunks: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract("css-loader")
+    }, {
+      test: /\.png$/,
+      loader: "file-loader"
+    }, {
+      test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/octet-stream"
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "file"
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=image/svg+xml"
+    }]
+  }
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,61 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  devtool: 'source-map',
+  entry: path.join(__dirname, '/src/main/resources'),
+  output: {
+    path: path.join(__dirname, './src/ratpack/react'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      'root.jQuery': 'jquery'
+    }),
+    new ExtractTextPlugin("bundle.css", { allChunks: true }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel?{"plugins":["babel-plugin-object-assign"],stage:0}'],
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract("css-loader")
+    }, {
+      test: /\.png$/,
+      loader: "file-loader"
+    }, {
+      test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/octet-stream"
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "file"
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=image/svg+xml"
+    }]
+  }
+};
